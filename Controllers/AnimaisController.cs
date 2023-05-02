72779bc023d6e26f71eaaf57511a93a6377a904c
DanielMonteiro22 <danielmontcosta55@gmail.com>
2023-05-01 23:37:18 -0300

    First commit

---
+++ Controllers/AnimaisController.cs
@@ -0,0 +1,45 @@
+﻿using Animais.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Animais.Controllers
+{
+    public class AnimaisController : Controller
+    {
+        public static List<AnimaisModel> pets = new List<AnimaisModel>
+        {
+            new AnimaisModel { id= 1, nome = "Bily", raca = "Vira-Lata", animal = "Cachorro"},
+
+            new AnimaisModel { id= 2, nome = "Megue", raca = "Vira-Lata", animal = "Cachorro"},
+
+            new AnimaisModel { id= 3, nome = "Sofia", raca = "Siames", animal = "Gato"},
+
+            new AnimaisModel { id= 4, nome = "Pudim", raca = "SRD", animal = "Gato"},
+
+            new AnimaisModel { id= 5, nome = "Cara-Preta", raca = "Bombaim", animal = "Gato"},
+
+            new AnimaisModel { id= 6, nome = "Branquela", raca = "Angorá", animal = "Gato"}
+
+        };
+
+        [HttpGet]
+        [Route("api/ListarPets")]
+
+        public IActionResult ListarTodosPets()
+        {
+            return Ok(pets);
+        }
+
+        [HttpGet]
+        [Route("api/ListarPets/{raca}")]
+
+        public IActionResult listarPetsRaca(string raca)
+        {
+            var especie = pets.Where(p => p.raca == raca);
+            if (especie == null)
+            {
+                return NoContent();
+            }
+            return Ok(especie);
+        }
+    }
+}
---
+++ Model/AnimaisModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Animais.Model
+{
+    public class AnimaisModel
+    {
+        public int id { get; set; }
+
+        public string nome { get; set; }
+
+        public string raca { get; set; }
+
+        public string animal { get; set; }
+    };
+}
