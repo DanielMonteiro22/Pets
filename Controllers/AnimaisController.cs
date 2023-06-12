a363818a66d5b6f3aa8f0d6abb760e540835c1ed
DanielMonteiro22 <danielmontcosta55@gmail.com>
2023-06-12 14:10:07 -0300

    Paginar Pets

--- Controllers/AnimaisController.cs
+++ Controllers/AnimaisController.cs
@@ -5,7 +5,10 @@ namespace Animais.Controllers
 {
     public class AnimaisController : Controller
     {
+        public const int ItemPag = 5;
+
         public static List<AnimaisModel> pets = new List<AnimaisModel>
+
         {
             new AnimaisModel { id= 1, nome = "Bily", raca = "Vira-Lata", animal = "Cachorro"},
 
@@ -19,7 +22,63 @@ namespace Animais.Controllers
 
             new AnimaisModel { id= 6, nome = "Branquela", raca = "Angorá", animal = "Gato"}
 
+
+
         };
+
+        [HttpGet]
+        [Route("api/PaginasPets")]
+        
+        public ActionResult<IEnumerable<string>> PaginarPets(int pagina = 1)
+        {
+            var totalPagina = (int)Math.Ceiling((double)pets.Count / ItemPag);
+            var currentPage = pagina > 0 ? pagina : 1;
+
+            var pagedPets = pets.Skip((currentPage - 1) * ItemPag).Take(ItemPag);
+                
+            var resultado = new
+            {
+                Pagina = currentPage,
+                totalPagina = totalPagina,
+                pets = pagedPets
+            };
+            return Ok(resultado);
+        }
+
+        //[HttpGet]
+        //[Route("api/PaginasPets")]
+
+        //public IActionResult PaginasPets (int page)
+        //{
+        //    int petsPerPage = 5;
+        //    int totalPets = pets.Count;
+        //    int totalPages = (int)Math.Ceiling((double)totalPets / ItemPag);
+
+        //    return Ok(pets);
+        //}
+
+        [HttpPost]
+        [Route("api/AdicionarPets")]
+        public IActionResult AdicionarPets([FromBody] AnimaisModel PetsNovos)
+        {
+            if(PetsNovos == null)
+            {
+                return BadRequest();
+            }
+            PetsNovos.id = pets.Count + 1;
+            pets.Add(PetsNovos);
+            return Ok(PetsNovos);
+        }
+
+        [HttpGet]
+        [Route("api/SumarioPets")]
+        public IActionResult SumarioPets()
+        {
+            var sumario = pets.GroupBy(p => p.animal)
+                              .Select(g => new { animal = g.Key, Quantidade = g.Count() })
+                              .ToDictionary(x => x.animal, x => x.Quantidade);
+            return Ok(sumario);
+        }
 
         [HttpGet]
         [Route("api/ListarPets")]
